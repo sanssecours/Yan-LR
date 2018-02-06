06528d0608c1bb11b0b37b81ccf4a00b3e6c0284
René Schwaiger <sanssecours@me.com>
2018-02-06 11:44:04 +0100

    Recognize tokens

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
+#include "TestLexer.h"
 #include "TestParser.h"
 #include <antlr4-runtime.h>
 #include <iostream>
 
+using namespace parser;
+using namespace antlr4;
+
 int main() {
-  std::cout << "Hello World" << std::endl;
+  ANTLRInputStream input(u8"hello world");
+  TestLexer lexer(&input);
+  CommonTokenStream tokens(&lexer);
+
+  tokens.fill();
+  for (auto token : tokens.getTokens()) {
+    std::cout << token->toString() << std::endl;
+  }
   return 0;
 }
