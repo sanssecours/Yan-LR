43626bfb687ad0f19d99bb14ff96e322ca872701
René Schwaiger <sanssecours@me.com>
2018-05-25 13:07:52 +0200

    Parser: Fix warnings about unused arguments

--- ErrorListener.hpp
+++ ErrorListener.hpp
@@ -7,10 +7,12 @@ using namespace std;
 class ErrorListener : public BaseErrorListener {
 
 private:
-  virtual void syntaxError(Recognizer *recognizer, Token *offendingSymbol,
+  virtual void syntaxError(Recognizer *recognizer __attribute__((unused)),
+                           Token *offendingSymbol __attribute__((unused)),
                            size_t line, size_t charPositionInLine,
                            const std::string &message,
-                           std::exception_ptr error) override {
+                           std::exception_ptr error
+                           __attribute__((unused))) override {
     cerr << line << ":" << charPositionInLine << " " << message << endl;
     exit(EXIT_FAILURE);
   }
