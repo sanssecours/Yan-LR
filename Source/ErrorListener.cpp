eecc5715dc71a1fbb07e0b5cc0df2465fbbc2c24
René Schwaiger <sanssecours@me.com>
2018-07-11 17:41:35 +0200

    Error Listener: Add Doxygen comments

--- Source/ErrorListener.cpp
+++ Source/ErrorListener.cpp
@@ -9,6 +9,21 @@ using std::endl;
 
 // -- Class --------------------------------------------------------------------
 
+/**
+ * @brief This method will be called if the parsing process fails.
+ *
+ * @param recognizer This parameter stores the current recognizer used to
+ *                   parse the input.
+ * @param offendingSymbol This token caused the failure of the parsing
+ *                        process.
+ * @param line This number specifies the line where the parsing process
+ *             failed.
+ * @param charPositionInLine This number specifies the character position in
+ *                           `line`, where the parsing process failed.
+ * @param message This text describes the parsing failure.
+ * @param error This parameter stores the exception caused by the parsing
+ *              failure.
+ */
 void ErrorListener::syntaxError(Recognizer *recognizer __attribute__((unused)),
                                 Token *offendingSymbol __attribute__((unused)),
                                 size_t line, size_t charPositionInLine,
--- Source/ErrorListener.hpp
+++ Source/ErrorListener.hpp
@@ -11,7 +11,26 @@ using std::string;
 
 // -- Class --------------------------------------------------------------------
 
+/**
+ * @brief This class specifies methods to alter error messages.
+ */
 class ErrorListener : public BaseErrorListener {
+
+  /**
+   * @brief This method will be called if the parsing process fails.
+   *
+   * @param recognizer This parameter stores the current recognizer used to
+   *                   parse the input.
+   * @param offendingSymbol This token caused the failure of the parsing
+   *                        process.
+   * @param line This number specifies the line where the parsing process
+   *             failed.
+   * @param charPositionInLine This number specifies the character position in
+   *                           `line`, where the parsing process failed.
+   * @param message This text describes the parsing failure.
+   * @param error This parameter stores the exception caused by the parsing
+   *              failure.
+   */
   void syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line,
                    size_t charPositionInLine, const string &message,
                    exception_ptr error);
